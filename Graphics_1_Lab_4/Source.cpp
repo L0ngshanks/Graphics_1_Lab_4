9e5bff31acd8060a836bea388f5de2a62926cfe7
L0ngshankz <haekaula@gmail.com>
2019-08-16 15:39:59 -0400

    Rotating Square

--- Graphics_1_Lab_4/Source.cpp
+++ Graphics_1_Lab_4/Source.cpp
@@ -5,50 +5,59 @@ int main(int argc, char** argv)
 {
 	RS_Initialize(RASTER_WIDTH, RASTER_HEIGHT);
 
-	VERTEX_4D square[12]
+	VERTEX_4D square[8]
 	{
-		//{ {-.25, -.25, .25, 1}, 0xFFFFFFFF},
-		//{ {.25, -.25, .25, 1}, 0xFFFFFFFF},
-		//{ {.25, .25, .25, 1}, 0xFFFFFFFF},
-		//{ {-.25, .25, .25, 1}, 0xFFFFFFFF},
+		{ {-.25, -.25, .25, 1}, 0xFFFFFFFF},
+		{ {.25, -.25, .25, 1}, 0xFFFFFFFF},
+		{ {.25, .25, .25, 1}, 0xFFFFFFFF},
+		{ {-.25, .25, .25, 1}, 0xFFFFFFFF},
 
-		//{ {}, 0xFFFFFFFF},
-		//{ {}, 0xFFFFFFFF},
-		//{ {}, 0xFFFFFFFF},
-		//{ {}, 0xFFFFFFFF},
-
-		//{ {}, 0xFFFFFFFF},
-		//{ {}, 0xFFFFFFFF},
-		//{ {}, 0xFFFFFFFF},
-		//{ {}, 0xFFFFFFFF};
-
+		{ {-.25, -.25, -.25, 1}, 0xFFFFFFFF},
+		{ {.25, -.25, -.25, 1}, 0xFFFFFFFF},
+		{ {.25, .25, -.25, 1}, 0xFFFFFFFF},
+		{ {-.25, .25, -.25, 1}, 0xFFFFFFFF},
 	};
 
-	VERTEX_4D test_inverse = { {7, 8, 5, 1}, 0xFFFFFFFF };
-	MATRIX_4D test_inverse_4x4 = { 1, 0, 0, 0,
-									0, 0.309, 0.951, 0,
-									0, -.951, 0.309, 0,
-									7, -2.282, 9.153, 1 };
 
-	test_inverse_4x4 = Fast_Inverse(test_inverse_4x4);
 
-	for(int x = 0; x < 4; ++x)
-		for (int y = 0; y < 4; ++y)
-		{
-			std::cout << test_inverse_4x4.mat[x][y] << std::endl;
-		}
+	double startTime = GetTickCount64();
+	float degree = 0;
 
 	while (RS_Update(Raster, NUM_PIXELS))
 	{
+		if (GetTickCount() - startTime >= 200)
+		{
+			startTime = 0;
+			if (degree > 360)
+				degree = 0.0f;
+			degree += .1f;
+		}
+
 		ClearScreen();
+
+		VertexShader = VS_World;
+		SV_WorldMatrix = Matrix_Rotation_4D_Y(degree);
 
 		PixelShader = PS_ChangeColor;
 		SP_Color = Red;
 
-		Parametric(square[0], square[1]);
-		Parametric(square[1], square[2]);
-		Parametric(square[2], square[3]);
-		Parametric(square[3], square[0]);
+		//Parametric(square[0], square[1]);
+		//Parametric(square[1], square[2]);
+		//Parametric(square[2], square[3]);
+		//Parametric(square[3], square[0]);
+
+		//Parametric(square[4], square[5]);
+		//Parametric(square[5], square[6]);
+		//Parametric(square[6], square[7]);
+		//Parametric(square[7], square[4]);
+
+		//Parametric(square[0], square[4]);
+		//Parametric(square[1], square[5]);
+		//Parametric(square[2], square[6]);
+		//Parametric(square[3], square[7]);
+
+
+
 
 	};
 
